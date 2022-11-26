201a5c9acb3bf65383c6df031f865b5788c67e12
Mikhail Bandurist <mikhail.bandurist@studio.unibo.it>
2022-11-26 18:53:26 +0100

    Version 9 (modified pd_to_np())

--- my_functions.py
+++ my_functions.py
@@ -8,22 +8,42 @@ from sklearn.linear_model import LinearRegression
 
 def load_data(file_name):
     """
-    Loads the data from a student dataset file_name and converts it to a training set (x_train, y_train).
-    The input x_train includes the features ["sex", "age", "Pstatus", "Mjob", "Fjob", "higher", "activities"],
-    the output y_train contains the final grade G3.
+    Loads the data from a student dataset file_name and extracts the columns 
+    ["school", "sex", "age", "Mjob", "Fjob", "higher", "activities", "G1", "G2", "G3"], where the column "G3" is the output 
+    of the model.
 
     Parameters:
-        file_name (string): path to a student dataset
+        file_name (string): Path to a student dataset
 
     Returns:
-        x_train (ndarray): Shape(m, 7), m - number of training examples (students) Input to the model
-        y_train (ndarray): Shape(m,) Output of the model
+        data (pandas dataframe): Extracted data (m - number of examples (rows), n - number of features (columns))
     """
     # importing the dataset
     data = pd.read_csv(file_name)
 
-    # Editing the raw dataset to get x_train and y_train
+    # Editing the raw dataset to get x and y
     data = data[["school", "sex", "age", "Mjob", "Fjob", "higher", "activities", "G1", "G2", "G3"]]
+    
+    return data
+
+
+
+
+def split_data(data):
+    """
+    
+    Converts data to input x and output y of the model. 
+    The input includes the features ["school", "sex", "age", "Mjob", "Fjob", "higher", "activities", "G1", "G2"].
+    The output contains the final grade "G3". 
+    In order to perfrom linear regression, the function turns categoraical features into dummy matrices.
+    
+    Parameters:
+        data (pandas dataframe): Data to be splitted into the input x to the model and the output y of the model.
+        
+    Retruns:
+        x (pandas dataframe): Input to the model
+        y (pandas dataframe): Output of the model
+    """
 
     #Eliminating rows with NaN values
     data = data.dropna()
@@ -44,33 +64,28 @@ def load_data(file_name):
             data = data.drop([column], axis=1)
 
     # Extracting x_train and y_train from the table
-    x_train = data.drop(["G3"], axis=1)
-    y_train = data["G3"]
+    x = data.drop(["G3"], axis=1)
+    y = data["G3"]
 
-    return x_train, y_train
+    return x, y
 
 
-def pd_to_np(x, y):
+def pd_to_np(x):
     """
 
     Converts a Dataframe to a Numpy array.
 
     Parameters:
         x (pandas dataframe): Training set as DataFrame
-        y (pandas dataframe): Output set as DataFrame
 
     Returns:
         x (ndarray): Training set as Numpy array
-        y (ndarray): Output set as Numpy array
     """
 
     x = x.to_numpy()
-    y = y.to_numpy()
-
     x = x.astype('float64')
-    y = y.astype('float64')
 
-    return x, y
+    return x
 
 
 
